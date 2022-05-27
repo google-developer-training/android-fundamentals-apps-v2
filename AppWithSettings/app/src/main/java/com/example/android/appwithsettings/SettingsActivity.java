c8b3224484561c9df8e703284c4293b387642ee6
pankaj.pal9 <pankaj.pal9@gmail.com>
2022-05-27 10:59:32 +0530

    Updated for Java 17, API 32 and Gradle 7.4.2

--- AppWithSettings/app/src/main/java/com/example/android/appwithsettings/SettingsActivity.java
+++ AppWithSettings/app/src/main/java/com/example/android/appwithsettings/SettingsActivity.java
@@ -16,7 +16,7 @@
 
 package com.example.android.appwithsettings;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 /**
